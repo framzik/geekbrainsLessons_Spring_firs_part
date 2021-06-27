707cc5e6d003dc8f8d9715c6b63fc049738938e9
framzik <framzik@gmail.com>
2021-06-27 21:16:21 +0300

    hw4

---
+++ lesson-4-spring-boot/src/main/java/ru/khrebtov/persist/Product.java
@@ -0,0 +1,39 @@
+package ru.khrebtov.persist;
+
+public class Product {
+    private Long id;
+    private String title;
+    private double cost;
+
+    public Product() {
+    }
+
+    public Product(String title, double cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
