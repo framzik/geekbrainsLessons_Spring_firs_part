639f4c0ad582074807fb733b319338cae6dd54c9
framzik <framzik@gmail.com>
2021-07-25 20:53:50 +0300

    ls 11 init

--- deploy/deploy-spring-boot-app.sh
+++ deploy/deploy-spring-boot-app.sh
@@ -6,8 +6,8 @@ apt install -y mysql-server openjdk-11-jdk maven nginx
 
 echo "!------ Configuring MySQL user ------!"
 mysql -u root -proot <<EOF
-CREATE USER 'my_user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON * . * TO 'my_user'@'localhost';
+CREATE USER 'framzik'@'localhost' IDENTIFIED BY 'framzik';
+GRANT ALL PRIVILEGES ON * . * TO 'framzik'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
